22f06d7118f241900f86be96ec6b44a0f3b4c46d
Khoerodin <khoerodin@live.com>
2018-12-21 06:11:27 +0700

    until video 18th

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
 describe ArticlesController do
+  subject { get :index }
+
   describe '#index' do
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
-      create_list :article, 2
-      get :index
-      json = JSON.parse(response.body)
-      pp json
-      json_data = json['data']
-      expect(json_data[0]['attributes']).to eq({
-        "title" => "My Awesome Article 1",
-        "content" => "The content of my awesome article 1",
-        "slug" => "my-awesome-article-1"
-      })
-      expect(json_data[1]['attributes']).to eq({
-        "title" => "My Awesome Article 2",
-        "content" => "The content of my awesome article 2",
-        "slug" => "my-awesome-article-2"
-      })  
+      articles = create_list :article, 2
+      subject
+      Article.recent.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+          "title" => article.title,
+          "content" => article.content,
+          "slug" => article.slug
+        })
+      end
+    end
+
+    it 'should return articles in the propper order' do
+      old_article = create :article
+      newer_article = create :article
+      subject
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
 
     end
   end
